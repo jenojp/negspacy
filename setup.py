a9b995ec5f7cbbceef6c8cb0ef035cfd7499329d
Jeno Pizarro <jjpizarr@us.ibm.com>
2019-08-14 18:57:57 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name = 'negspacy',
-    version = 0.1.0,
+    version = '0.1.0',
     url = 'https://github.com/jenojp/negspacy',
     author = 'Jeno Pizarro',
     author_email = 'jenopizzaro@gmail.com',
